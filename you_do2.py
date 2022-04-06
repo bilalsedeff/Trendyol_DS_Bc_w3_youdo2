51462686d52adf3d904eb3d6430a9ac8a0db351a
bilalsedeff <bilalsedef@gmail.com>
2022-04-06 16:59:53 +0300

    Small changes

--- you_do2.py
+++ you_do2.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 from time import sleep
 
 ##
+# We initialize the dataset
 df = pd.read_csv('https://files.grouplens.org/datasets/movielens/ml-100k/u.data', delimiter=r'\t',
                  names=['user_id', 'item_id', 'rating', 'timestamp'], engine='python')
 r_test = df.pivot(index='user_id', columns='item_id', values='rating').values
@@ -16,8 +17,9 @@ r_copy = r_test.copy()
 
 irow, jcol = np.where(~np.isnan(r_copy))
 
-##
 
+##
+# We randomly select not nan entries and mask them with nan values, to predict them later
 idx = np.random.choice(np.arange(100000), size=1000, replace=False)
 test_irow = irow[idx]
 test_jcol = jcol[idx]
@@ -28,7 +30,7 @@ for i in test_irow:
 
 
 ##
-# Pearson similarity
+# We apply Pearson similarity between users to create User-User similarity matrix later
 def sim(r_: np.ndarray, i: int, j: int, mask: np.ndarray, mu: np.ndarray) -> float:
     num = 0.
     denum = 0.
@@ -44,6 +46,7 @@ def sim(r_: np.ndarray, i: int, j: int, mask: np.ndarray, mu: np.ndarray) -> flo
 
 
 ##
+# Below function is the similarity matrix constructor
 def sim_matrix(r_: np.ndarray) -> np.ndarray:
     mu = np.nanmean(r_, axis=1)
     sim_matrix = np.zeros((r_.shape[0], r_.shape[0]))
@@ -57,10 +60,12 @@ def sim_matrix(r_: np.ndarray) -> np.ndarray:
 
 
 ##
+# We construct our similarity matrix
 sim_matrix = sim_matrix(r_copy)
 
 
 ##
+# Top n similar users who voted item j to user i for r_ij
 def top_n(r_: np.ndarray, i: int, j: int, n: int) -> np.ndarray:
     other_users_idx = np.asarray(np.where(~np.isnan(r_[:, j])))[0]
     other_users_idx = other_users_idx[other_users_idx != i]
@@ -69,6 +74,7 @@ def top_n(r_: np.ndarray, i: int, j: int, n: int) -> np.ndarray:
 
 
 ##
+# Below function randomly creates [1,n] shaped vector which will be used for predicting weights for r_hat prediction
 def w(top_n: int) -> np.ndarray:
     rnd = np.random.rand(top_n)
     rnd = rnd * 2 - 1
@@ -76,6 +82,7 @@ def w(top_n: int) -> np.ndarray:
 
 
 ##
+# Below function predicts r_hat using both top_n user information and predicted weights
 def r_hat(r_: np.ndarray, i: int, j: int, mu: np.ndarray, w: np.ndarray, top_n_user, k) -> float:
     top_n_user_ = top_n_user
     top_n_user_list = top_n(r_, i, j, k)
@@ -85,11 +92,13 @@ def r_hat(r_: np.ndarray, i: int, j: int, mu: np.ndarray, w: np.ndarray, top_n_u
 
 
 ##
+# Loss function
 def loss(r_: np.ndarray, i: int, j: int, mu: np.ndarray, w: np.ndarray, top_n_user, k) -> float:
     return np.sum(np.sum((r_[i, j] - r_hat(r_, i, j, mu, w, top_n_user, k)) ** 2)) / r_.shape[0]
 
 
 ##
+# The gradient scalar value to be used in gradient descent. It is the derivative of loss function with respect to w_un
 def gradient(r_: np.ndarray, i: int, j: int, mu: np.ndarray, w_: np.ndarray, top_n_users: int) -> float:
     if np.isnan(r_[i, j]):
         r_[i, j] = 0
@@ -99,6 +108,7 @@ def gradient(r_: np.ndarray, i: int, j: int, mu: np.ndarray, w_: np.ndarray, top
 
 
 ##
+# Below function updates the w_un according to the gradient function output
 def gradient_descent(r_: np.ndarray, i: int, j: int, mu: np.ndarray, w_: np.ndarray,
                      alpha: float, top_n_users: int) -> np.ndarray:
     gradient_ = gradient(r_, i, j, mu, w_, top_n_users)
@@ -107,11 +117,13 @@ def gradient_descent(r_: np.ndarray, i: int, j: int, mu: np.ndarray, w_: np.ndar
 
 
 ##
+# The erorr function which calculates the difference between r_ij and r_hat
 def error(r_ij, r_hat):
     return (r_ij - r_hat) ** 2
 
 
 ##
+# Below function considers all r_test and r_train matrix to calculate total loss
 def total_loss(r_test, r_train):
     total_loss = 0.
     for i in range(r_test.shape[0]):
@@ -122,6 +134,8 @@ def total_loss(r_test, r_train):
 
 
 ##
+# Below is the weight matrix which has r_train.shape[0] rows and top_n columns. Those values are to be optimized
+# using gradient descent function
 def w_matrix(r_: np.ndarray, k):
     matrix = (r_.shape[0], k)
     matrix = np.zeros(matrix)
@@ -129,10 +143,12 @@ def w_matrix(r_: np.ndarray, k):
 
 
 ##
+# We construct the weight matrix which has the shape of [r_train.shape[0], top_n)
 matrix_ = w_matrix(r_copy, 4)
 
 
 ##
+# The function for to fill the weight matrix with random values between -1 to 1
 def fill_w_matrix(matrix, k):
     for i in range(matrix.shape[0]):
         w_ = w(k)
@@ -143,11 +159,13 @@ def fill_w_matrix(matrix, k):
 
 
 ##
+# We fill the weight matrix
 matrix_ = fill_w_matrix(matrix_, 4)
 
 
 ##
-def fit(r_test: np.ndarray, r_: np.ndarray, k, epoch, alpha, matrix_: np.ndarray):
+# The fit function. It does the fit and the prediction in the same loop for computing time concerns
+def fit_predict(r_test: np.ndarray, r_: np.ndarray, k, epoch, alpha, matrix_: np.ndarray):
     mu = np.nanmean(r_, axis=1)
     m, n = r_.shape
     m = list(range(m))
@@ -185,7 +203,7 @@ def main():
     k = 4
     epoch_ = 20
     alpha_ = 0.00001
-    results = fit(r_test, r_copy, k, epoch_, alpha_, matrix_)
+    results = fit_predict(r_test, r_copy, k, epoch_, alpha_, matrix_)
     predicted_matrix = results[0]
     performance = results[1]
     matrix = results[2]
@@ -195,6 +213,7 @@ def main():
 
 ##
 if __name__ == "__main__":
+    results = main()
     fitted_r_train = main()[0]
     performance = main()[1]
     matrix_ = main()[2]
@@ -202,5 +221,6 @@ if __name__ == "__main__":
 
 ##
 print("Final Loss: ", total_loss(r_test, fitted_r_train))
+print(performance)
 
 ##
